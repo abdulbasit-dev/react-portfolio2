5a98569b871cfc6750b2271b144df3e0a21fd0a5
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-12-07 15:21:11 +0300

    fix social media css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ import SocialContact from './SocialContact';
 function Header({data}) {
   console.log(data);
   return (
-    <div className='header bg-dar p-2 '>
-      <div className='hed d-flex justify-content-between align-items-center'>
+    <div className='header p-2 '>
+      <div className='header-container d-flex justify-content-between align-items-center'>
         <nav className='borderYtoX mt-3'>
-          <ul className='d-flex'>
+          <ul className='d-flex list-item'>
             <li className='nav-item'>
               <a href='#aboutMe' className='nav-link'>
                 ABOUT ME
@@ -22,6 +22,11 @@ function Header({data}) {
             <li className='nav-item'>
               <a href='#projects' className='nav-link'>
                 PROJECTS
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a href='#certificate' className='nav-link'>
+                certificate
               </a>
             </li>
             <li className='nav-item'>
--- src/components/SocialContact.jsx
+++ src/components/SocialContact.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './SocialContact.css';
+
 function SocialContact({data}) {
   return (
-    <div className='header__socialIcon text-white'>
+    <div className='socialIcon text-white'>
       {data.social.map(icon => (
         <a href={icon.url} key={icon.className} target='_blanck'>
           <i className={`${icon.className} mr-4`}></i>
